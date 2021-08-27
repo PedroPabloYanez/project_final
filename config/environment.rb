b82611258277f18855800edf0e30ec304dd506c3
pedro <pedropablo.yanez@hotmail.com>
2021-08-27 19:08:09 -0400

    fix mailer ADD ENV

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+    :password => ENV["MAILER_SECRET_KEY"], # This is the secret sendgrid API key which was issued during API key creation
     :domain => 'https://sheltered-waters-98252.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
