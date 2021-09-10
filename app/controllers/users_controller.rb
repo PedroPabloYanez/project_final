fd027390cbe48909cd8ca4c655b7996a51254e3f
pedro <pedropablo.yanez@hotmail.com>
2021-09-10 14:16:04 -0300

    controller home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
     def index
         #@users = User.all
         @users = User.order(:user_name).page(params[:page]).per(10)
